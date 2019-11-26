c2c30a4f43f525357b1e9bb1b13472b500bc67b5
a.nikiforov <a.nikiforov@magdv.com>
2019-11-26 16:21:05 +0700

    move npm-run-all to dependencies

--- client/index.js
+++ client/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { hot } from 'react-hot-loader/root';
+import { hot } from "react-hot-loader/root";
 import Root from "./components/App/index.jsx";
 
-import './styles/main.scss';
+import "./styles/main.scss";
 
-const App = hot(Root);
+const App = process.env.NODE_ENV === "development" ? hot(Root) : Root;
 
 ReactDOM.render(<App />, document.getElementById("app"));
